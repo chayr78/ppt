8b774c5d940856017c415078bfa5de69ae18a110
chayr78 <mg8myrvccp@privaterelay.appleid.com>
2026-05-22 14:54:01 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-eleccion ="";
-function jugar(opcionSeleccionada){
-    eleccion=opcionSeleccionada;
+let eleccion = "";
+
+function jugar(opcionSeleccionada) {
+
+    eleccion = opcionSeleccionada;
+
     let eleccioncom = random();
-    gp(eleccion,eleccioncom);
+
+    gp(eleccion, eleccioncom);
 }
-function random (){
-    let piedra="piedra",papel="papel",tijeras="tijeras";
-    let play=Math.floor(Math.random()*3);
-    if(play==0){
+
+function random() {
+
+    let piedra = "piedra",
+        papel = "papel",
+        tijeras = "tijeras";
+
+    let play = Math.floor(Math.random() * 3);
+
+    if (play == 0) {
         return piedra;
-    }else if(play==1){
+    } else if (play == 1) {
         return papel;
-    }else{
+    } else {
         return tijeras;
-    }}
-    function gp(eleccion,eleccioncom) {
-        let res;
-        if (eleccioncom==eleccion){
-            res=("Empate")
-        }
-        else if (eleccion=="piedra"&&eleccioncom=="tijeras"||
-                eleccion=="papel"&&eleccioncom=="piedra"||
-                eleccion=="tijeras"&&eleccioncom=="papel"
-        ){
-        res=("Ganaste")
-        }else{
-            res=("Perdiste")
-        }
-        document.getElementById("resultado").innerHTML = res;
     }
+}
+
+function gp(eleccion, eleccioncom) {
+
+    let res;
+
+    if (eleccioncom == eleccion) {
+
+        res = "🤝 Empate";
+
+    } else if (
+        eleccion == "piedra" && eleccioncom == "tijeras" ||
+        eleccion == "papel" && eleccioncom == "piedra" ||
+        eleccion == "tijeras" && eleccioncom == "papel"
+    ) {
+
+        res = "🎉 Ganaste";
+
+    } else {
+
+        res = "😢 Perdiste";
+    }
+
+    document.getElementById("resultado").innerHTML = res;
+}
